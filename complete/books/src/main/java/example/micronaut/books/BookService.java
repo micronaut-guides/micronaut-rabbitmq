d3324ed1af182c71b85b70e2f7c1eaf4e89b2372
Iván López <lopez.ivan@gmail.com>
2019-02-15 15:56:47 +0100

    Make method public in BookService and init private

--- complete/books/src/main/java/example/micronaut/books/BookService.java
+++ complete/books/src/main/java/example/micronaut/books/BookService.java
@@ -13,17 +13,17 @@ public class BookService {
     private static final List<Book> bookStore = new ArrayList<>();
 
     @PostConstruct
-    void init() {
+    private void init() {
         bookStore.add(new Book("1491950358", "Building Microservices"));
         bookStore.add(new Book("1680502395", "Release It!"));
         bookStore.add(new Book("0321601912", "Continuous Delivery"));
     }
 
-    List<Book> listAll() {
+    public List<Book> listAll() {
         return bookStore;
     }
 
-    Optional<Book> findByIsbn(String isbn) {
+    public Optional<Book> findByIsbn(String isbn) {
         return bookStore.stream()
                 .filter(b -> b.getIsbn().equals(isbn))
                 .findFirst();
