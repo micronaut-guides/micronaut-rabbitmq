033060c2e4fc91451321efe60fb54f328f98a234
Iván López <lopez.ivan@gmail.com>
2019-02-19 12:39:40 +0100

    Simplify ChannelPoolListener

--- complete/analytics/src/main/java/example/micronaut/analytics/ChannelPoolListener.java
+++ complete/analytics/src/main/java/example/micronaut/analytics/ChannelPoolListener.java
@@ -2,40 +2,18 @@ package example.micronaut.analytics;
 
 import com.rabbitmq.client.BuiltinExchangeType;
 import com.rabbitmq.client.Channel;
-import io.micronaut.configuration.rabbitmq.connect.ChannelPool;
-import io.micronaut.context.event.BeanCreatedEvent;
-import io.micronaut.context.event.BeanCreatedEventListener;
+import io.micronaut.configuration.rabbitmq.connect.ChannelInitializer;
 
 import javax.inject.Singleton;
 import java.io.IOException;
 
 @Singleton
-public class ChannelPoolListener implements BeanCreatedEventListener<ChannelPool> {
+public class ChannelPoolListener extends ChannelInitializer {
 
     @Override
-    public ChannelPool onCreated(BeanCreatedEvent<ChannelPool> event) {
-        ChannelPool pool = event.getBean();
-
-        Channel channel = null;
-        try {
-            channel = pool.getChannel();
-        } catch (IOException e) {
-            // The channel couldn't be retrieved
-        }
-
-        if (channel != null) {
-            try {
-                channel.exchangeDeclare("micronaut", BuiltinExchangeType.DIRECT, true);
-
-                channel.queueDeclare("analytics", true, false, false, null);
-                channel.queueBind("analytics", "micronaut", "analytics");
-            } catch (IOException e) {
-                // no-op
-            } finally {
-                pool.returnChannel(channel);
-            }
-        }
-
-        return pool;
+    public void initialize(Channel channel) throws IOException {
+        channel.exchangeDeclare("micronaut", BuiltinExchangeType.DIRECT, true);
+        channel.queueDeclare("analytics", true, false, false, null);
+        channel.queueBind("analytics", "micronaut", "analytics");
     }
 }
--- complete/books/src/main/java/example/micronaut/books/ChannelPoolListener.java
+++ complete/books/src/main/java/example/micronaut/books/ChannelPoolListener.java
@@ -2,40 +2,18 @@ package example.micronaut.books;
 
 import com.rabbitmq.client.BuiltinExchangeType;
 import com.rabbitmq.client.Channel;
-import io.micronaut.configuration.rabbitmq.connect.ChannelPool;
-import io.micronaut.context.event.BeanCreatedEvent;
-import io.micronaut.context.event.BeanCreatedEventListener;
+import io.micronaut.configuration.rabbitmq.connect.ChannelInitializer;
 
 import javax.inject.Singleton;
 import java.io.IOException;
 
 @Singleton
-public class ChannelPoolListener implements BeanCreatedEventListener<ChannelPool> {
+public class ChannelPoolListener extends ChannelInitializer {
 
     @Override
-    public ChannelPool onCreated(BeanCreatedEvent<ChannelPool> event) {
-        ChannelPool pool = event.getBean();
-
-        Channel channel = null;
-        try {
-            channel = pool.getChannel();
-        } catch (IOException e) {
-            // The channel couldn't be retrieved
-        }
-
-        if (channel != null) {
-            try {
-                channel.exchangeDeclare("micronaut", BuiltinExchangeType.DIRECT, true); // <1>
-
-                channel.queueDeclare("analytics", true, false, false, null); // <2>
-                channel.queueBind("analytics", "micronaut", "analytics"); // <3>
-            } catch (IOException e) {
-                // no-op
-            } finally {
-                pool.returnChannel(channel);
-            }
-        }
-
-        return pool;
+    public void initialize(Channel channel) throws IOException {
+        channel.exchangeDeclare("micronaut", BuiltinExchangeType.DIRECT, true); // <1>
+        channel.queueDeclare("analytics", true, false, false, null); // <2>
+        channel.queueBind("analytics", "micronaut", "analytics"); // <3>
     }
 }
