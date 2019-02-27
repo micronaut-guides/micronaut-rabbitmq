08e025cebcf6355e31cbedc2c2c89b652f9632a7
Iván López <lopez.ivan@gmail.com>
2019-02-27 09:39:43 +0100

    Add Graal support

---
+++ complete/analytics/build-native-image.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+./gradlew assemble
+
+#java -cp build/libs/analytics-0.1.jar io.micronaut.graal.reflect.GraalClassLoadingAnalyzer
+
+native-image --no-server \
+             --class-path build/libs/analytics-0.1.jar \
+             -H:ReflectionConfigurationFiles=reflect.json \
+             -H:EnableURLProtocols=http \
+             -H:IncludeResources="logback.xml|application.yml" \
+             -H:Name=analytics \
+             -H:Class=example.micronaut.analytics.Application \
+             -H:+ReportUnsupportedElementsAtRuntime \
+             -H:+AllowVMInspection \
+             --allow-incomplete-classpath \
+             --rerun-class-initialization-at-runtime='sun.security.jca.JCAUtil$CachedSecureRandomHolder,javax.net.ssl.SSLContext' \
+             --delay-class-initialization-to-runtime=io.netty.handler.codec.http.HttpObjectEncoder,io.netty.handler.codec.http.websocketx.WebSocket00FrameEncoder,io.netty.handler.ssl.util.ThreadLocalInsecureRandom,com.sun.jndi.dns.DnsClient
