5df16e5353f84156692ae3831a18f62f27f54049
Iván López <lopezi@objectcomputing.com>
2019-11-20 16:10:31 +0100

    Fix travis scripts

--- travis-build-pr.sh
+++ travis-build-pr.sh
@@ -6,13 +6,13 @@ export EXIT_STATUS=0
 echo "Executing tests for branch $TRAVIS_BRANCH"
 ./gradlew --console=plain clean
 
-./gradlew --console=plain complete/books:test || EXIT_STATUS=$?
+./gradlew --console=plain complete:books:test || EXIT_STATUS=$?
 
 if [[ $EXIT_STATUS -ne 0 ]]; then
   exit $EXIT_STATUS
 fi
 
-./gradlew --console=plain complete/analytics:test || EXIT_STATUS=$?
+./gradlew --console=plain complete:analytics:test || EXIT_STATUS=$?
 
 if [[ $EXIT_STATUS -ne 0 ]]; then
   exit $EXIT_STATUS
--- travis-build.sh
+++ travis-build.sh
@@ -3,13 +3,13 @@ set -e
 
 export EXIT_STATUS=0
 
-./gradlew --console=plain complete/books:test || EXIT_STATUS=$?
+./gradlew --console=plain complete:books:test || EXIT_STATUS=$?
 
 if [[ $EXIT_STATUS -ne 0 ]]; then
   exit $EXIT_STATUS
 fi
 
-./gradlew --console=plain complete/analytics:test || EXIT_STATUS=$?
+./gradlew --console=plain complete:analytics:test || EXIT_STATUS=$?
 
 if [[ $EXIT_STATUS -ne 0 ]]; then
   exit $EXIT_STATUS
